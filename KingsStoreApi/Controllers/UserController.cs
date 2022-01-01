170b63f934021125623522beabb71bd9f90acdcc
ogubuike Alex <ogubuikealex@gmail.com>
2022-01-02 00:52:02 +0100

    Added 18 endpoints to the user controller

--- KingsStoreApi/Controllers/UserController.cs
+++ KingsStoreApi/Controllers/UserController.cs
@@ -1,8 +1,10 @@
 ﻿using KingsStoreApi.Data.Implementations;
-using KingsStoreApi.Extensions;
+using KingsStoreApi.Data.Interfaces;
+using KingsStoreApi.Model.DataTransferObjects.SharedDTO;
 using KingsStoreApi.Model.DataTransferObjects.UserServiceDTO;
 using KingsStoreApi.Model.Entities;
 using KingsStoreApi.Services.Interfaces;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -15,18 +17,21 @@ namespace KingsStoreApi.Controllers
     public class UserController : ControllerBase
     {
         private readonly UserManager<User> _userManager;
-        private readonly AuthenticationManager _authenticationManager;
+        private readonly IAuthenticationManager _authenticationManager;
         private readonly SignInManager<User> _signInManager;
         private readonly IUserService _userService;
+        private readonly IWebHostEnvironment _environment;
 
-        public UserController(UserManager<User> userManager, AuthenticationManager authenticationManager, SignInManager<User> signInManager, IUserService service)
+        public UserController(UserManager<User> userManager, IAuthenticationManager authenticationManager, SignInManager<User> signInManager, IUserService service, IWebHostEnvironment webHostEnvironment)
         {
+            _environment = webHostEnvironment;
             _userManager = userManager;
             _authenticationManager = authenticationManager;
             _signInManager = signInManager;
-           _userService = service;
+            _userService = service;
         }
 
+        [HttpGet("{email}")]
         public async Task<IActionResult> GetUser(string email)
         {
             var result = await _userService.GetUserAsync(email);
@@ -38,6 +43,7 @@ namespace KingsStoreApi.Controllers
             return Ok(user);
         }
 
+        [HttpGet]
         public IActionResult GetAllUsers()
         {
             var result = _userService.GetAllUsers();
@@ -48,13 +54,123 @@ namespace KingsStoreApi.Controllers
             var users = result.Object as IEnumerable<User>;
             return Ok(users);
         }
+        [HttpGet("vendors")]
+        public IActionResult GetAllVendors()
+        {
+            var result = _userService.GetAllVendors();
 
+            if (!result.Success)
+                return NotFound(result.Message);
+            var vendors = result.Object as IEnumerable<User>;
+
+            return Ok(vendors);
+        }
+        [HttpGet("activeUsers")]
+        public IActionResult GetAllActiveUsers()
+        {
+            var result = _userService.GetAllActiveUsers();
+
+            if (!result.Success)
+                return NotFound(result.Message);
+            var users = result.Object as IEnumerable<User>;
+
+            return Ok(users);
+        }
+        [HttpGet("customers")]
+        public IActionResult GetAllCustomers()
+        {
+            var result = _userService.GetAllCustomers();
+
+            if (!result.Success)
+                return NotFound(result.Message);
+            var users = result.Object as IEnumerable<User>;
+
+            return Ok(users);
+        }
+        [HttpPost("removeVendor")]
+        public async Task<IActionResult> UnmakeUserAVendorAsync(string email)
+        {
+            var result = await _userService.UnMakeUserAVendorAsync(email);
+
+            if (!result.Success)
+                return NotFound(result.Message);
+
+            return Ok(result.Message);
+        }
+
+        [HttpPost("makeAdmin")]
+        public async Task<IActionResult> MakeUserAnAdminAsync(string email)
+        {
+            var result = await _userService.MakeUserAnAdminAsync(email);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+
+            return Ok(result.Message);
+        }
+
+        [HttpPost("removeAdmin")]
+        public async Task<IActionResult> UnmakeUserAnAdminAsync(string email)
+        {
+            var result = await _userService.UnMakeUserAnAdminAsync(email);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+
+            return Ok(result.Message);
+        }
+
+        [HttpPost("updatePic")]
+        public async Task<IActionResult> UpdateUserProfilePic(UploadImageDTO model)
+        {
+            var result = await _userService.UpdateUserProfilePic(model);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+
+            return Ok(result.Message);
+        }
+
+        [HttpPost("updateBio")]
+        public async Task<IActionResult> UpdateUserBio(string newBio)
+        {
+            var result = await _userService.UpdateUserBio(newBio);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+
+            return Ok(result.Message);
+        }
+
+        [HttpPost("updateName")]
+        public async Task<IActionResult> UpdateUserFullName(UpdateFullNameDTO model)
+        {
+            var result = await _userService.UpdateUserFullName(model);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+
+            return Ok(result.Message);
+        }
+
+        [HttpPost("removePic")]
+        public async Task<IActionResult> RemoveUserProfilePicture(string email)
+        {
+            var result = await _userService.RemoveProfilePicture(email);
+
+            if (!result.Success)
+                return BadRequest(result.Message);
+
+            return Ok(result.Message);
+        }
+
+        [HttpPost]
         public async Task<IActionResult> Login(LogInDTO model)
         {
             var result = await _userService.LogIn(model);
             if (!result.Success)
             {
-                return result.Message.Contains("User Not found") ? 
+                return result.Message.Contains("User Not found") ?
                     NotFound(result.Message) : Unauthorized(result.Message);
             }
 
@@ -62,12 +178,14 @@ namespace KingsStoreApi.Controllers
             return Ok(token);
         }
 
+        [HttpPost("logout")]
         public async Task<IActionResult> LogOut()
         {
             await _signInManager.SignOutAsync();
             return NoContent();
         }
 
+        [HttpPost("register")]
         public async Task<IActionResult> Register(RegisterDTO model)
         {
             var registrationResult = await _userService.RegisterAsync(model);
@@ -78,6 +196,7 @@ namespace KingsStoreApi.Controllers
             return Ok(registrationResult.Message);
         }
 
+        [HttpPost("makeVendor")]
         public async Task<IActionResult> MakeUserAVendorAsync(string email)
         {
             var result = await _userService.MakeUserAVendorAsync(email);
@@ -91,6 +210,7 @@ namespace KingsStoreApi.Controllers
             return Ok(result.Message);
         }
 
+        [HttpPost("toggleActiveStatus")]
         public async Task<IActionResult> ToggleUserActivationStatusAsync(string email)
         {
             var result = await _userService.ToggleUserActivationStatusAsync(email);
@@ -98,14 +218,14 @@ namespace KingsStoreApi.Controllers
                 return NotFound(result.Message);
 
             return Ok(result.Message);
-        } 
+        }
 
-        public async Task<IActionResult> ToggleUserSoftDeleteAsync (string email)
+        [HttpPost("toggleSoftDelete")]
+        public async Task<IActionResult> ToggleUserSoftDeleteAsync(string email)
         {
             var result = await _userService.ToggleUserSoftDeleteAsync(email);
 
-            return Ok(result.Message); 
+            return Ok(result.Message);
         }
-
     }
 }
