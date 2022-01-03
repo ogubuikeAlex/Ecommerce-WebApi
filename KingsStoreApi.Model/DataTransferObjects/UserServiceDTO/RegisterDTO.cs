43dfc7561afacbd8bbf14e0e21b5f0cbdd5c03ed
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-03 04:01:22 +0100

    Added ValidateUserDTO and reformed RegisterDTO

--- KingsStoreApi.Model/DataTransferObjects/UserServiceDTO/RegisterDTO.cs
+++ KingsStoreApi.Model/DataTransferObjects/UserServiceDTO/RegisterDTO.cs
@@ -5,16 +5,9 @@ namespace KingsStoreApi.Model.DataTransferObjects.UserServiceDTO
     public class RegisterDTO
     {
         public string FullName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public bool isAdmin { get; set; }
-        public bool isVendor { get; set; }
-        public DateTime LastLogin { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-        public byte[] ProfilePicture { get; set; }
         public string Bio { get; set; }
+        public  string PhoneNumber { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
-        public bool isActive { get; set; }
     }
 }
---
+++ KingsStoreApi.Model/DataTransferObjects/UserServiceDTO/ValidateUserDTO.cs
@@ -0,0 +1,10 @@
+﻿using KingsStoreApi.Model.Entities;
+
+namespace KingsStoreApi.Model.DataTransferObjects.UserServiceDTO
+{
+    public class ValidateUserDTO
+    {
+        public User User { get; set; }
+        public string Password { get; set; }
+    }
+}
