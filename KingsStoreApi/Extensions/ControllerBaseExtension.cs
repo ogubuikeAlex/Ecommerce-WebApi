17f447fe2b50e03aef3f952d8287f127c3ec4a64
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-06 23:36:25 +0100

    Extended Controllerbase so that all controllers will be able to get current loggedIn user info

---
+++ KingsStoreApi/Extensions/ControllerBaseExtension.cs
@@ -0,0 +1,23 @@
+﻿using KingsStoreApi.Model.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace KingsStoreApi.Extensions
+{
+    public class ControllerBaseExtension : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public ControllerBaseExtension(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<User> GetLoggedInUserAsync()
+        {
+            var (userId, userEmail) = HttpContext.User.GetLoggedInUserInfo();
+            var user = await _userManager.FindByNameAsync(userEmail);
+            return user;
+        }
+    }
+}
