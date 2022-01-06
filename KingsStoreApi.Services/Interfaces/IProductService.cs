48d21fd0f421621c5bf9be1104bae017cd73aaa0
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-06 20:16:46 +0100

    Updated the IProduct service to reflect changes

--- KingsStoreApi.Services/Interfaces/IProductService.cs
+++ KingsStoreApi.Services/Interfaces/IProductService.cs
@@ -1,6 +1,7 @@
 ﻿using KingsStoreApi.Helpers.Implementations;
 using KingsStoreApi.Model.DataTransferObjects.ProductServiceDTO;
 using KingsStoreApi.Model.DataTransferObjects.SharedDTO;
+using System.Threading.Tasks;
 
 namespace KingsStoreApi.Services.Interfaces
 {
@@ -9,12 +10,15 @@ namespace KingsStoreApi.Services.Interfaces
         ReturnModel GetProductByName(string name);
         ReturnModel GetProductById(string id);
         ReturnModel GetAllProducts();
+        Task<ReturnModel> GetDiabledProductsByVendor(string email);
+        Task<ReturnModel> GetProductsByVendor(string email);
+        Task<ReturnModel> BuyNow();
         ReturnModel UploadProduct(UploadProductDTO model);
-        ReturnModel RemoveProduct(RemoveProductDTO model);
         ReturnModel UplaodProductImage(UploadImageDTO model);//patch
         ReturnModel EditProductPrice(EditProductPriceDTO model);//patch
         ReturnModel EditProductSummary(EditProductSummaryDTO model);//patch
         ReturnModel EditProductTitle(EditProductTitleDTO model);//patch
-        ReturnModel SoftDeleteProduct(DeleteProductDTO model);        
+        Task<ReturnModel> TemporarilyDisableAProduct(string id);
+        Task<ReturnModel> PermanentlyDisableAProduct(DeleteProductDTO model);
     }
 }
