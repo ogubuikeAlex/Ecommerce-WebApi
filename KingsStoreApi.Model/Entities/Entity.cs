60e5dd42ed45b957f06c4df572fec8a1c63ec7c6
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-06 22:58:17 +0100

    Added logic to be able to internally set the permanent record of an entity's delete status

--- KingsStoreApi.Model/Entities/Entity.cs
+++ KingsStoreApi.Model/Entities/Entity.cs
@@ -3,11 +3,22 @@ using System;
 
 namespace KingsStoreApi.Model.Entities
 {
-    public class Entity : ISoftDelete
+    public class Entity : IDelete
     {
+        private static DateTime _isDeletedTimeStamp;
+        private bool _hasBeenDeletedOver30Days;
+        public DateTime? IsDeletedTimeStamp { get; set; }
+
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
-        public bool IsDeleted { get ; set ; }
-
+        public bool IsDeleted { get; set; }
+        public bool HasBeenRemovedOver30Days
+        {
+            get => _hasBeenDeletedOver30Days;
+            set
+            {
+                _hasBeenDeletedOver30Days = _isDeletedTimeStamp - DateTime.Now >= TimeSpan.FromDays(30);
+            }
+        }
     }
 }
