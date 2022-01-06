9261e4a6423ce8d4440d6cb628c54eba65716140
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-06 22:56:50 +0100

    Update to product to allow show of perm delete

--- KingsStoreApi.Model/Entities/Product.cs
+++ KingsStoreApi.Model/Entities/Product.cs
@@ -9,9 +9,13 @@ namespace KingsStoreApi.Model.Entities
         public string Title { get; set; }
         public string Summary { get; set; }
         public decimal Price { get; set; }
-        public bool hasBeenRemovedOver30Days { get; set; }
+      
         public int Quantity { get; set; }
         public DateTime PublishedAt { get; set; }
         public byte[] ProductImage { get; set; }
+
+        //Store date of isdeleted
+        //if after 30 dyas product is still isdeleted
+        //turn on 
     }
 }
