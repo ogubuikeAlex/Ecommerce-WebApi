e779aacd39a1f7a9a1609d628a774a0f36e3f0a4
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-07 21:04:45 +0100

    Changed method signatures to match implementation

--- KingsStoreApi.Services/Interfaces/IProductService.cs
+++ KingsStoreApi.Services/Interfaces/IProductService.cs
@@ -1,6 +1,7 @@
 ﻿using KingsStoreApi.Helpers.Implementations;
 using KingsStoreApi.Model.DataTransferObjects.ProductServiceDTO;
 using KingsStoreApi.Model.DataTransferObjects.SharedDTO;
+using KingsStoreApi.Model.Entities;
 using System.Threading.Tasks;
 
 namespace KingsStoreApi.Services.Interfaces
@@ -10,14 +11,14 @@ namespace KingsStoreApi.Services.Interfaces
         ReturnModel GetProductByName(string name);
         ReturnModel GetProductById(string id);
         ReturnModel GetAllProducts();
-        Task<ReturnModel> GetDiabledProductsByVendor(string email);
+        Task<ReturnModel> GetDisabledProductsByVendor(string email);
         Task<ReturnModel> GetProductsByVendor(string email);
         Task<ReturnModel> BuyNow();
         ReturnModel UploadProduct(UploadProductDTO model);
-        ReturnModel UplaodProductImage(UploadImageDTO model);//patch
-        ReturnModel EditProductPrice(EditProductDTO model);//patch
-        ReturnModel EditProductSummary(EditProductSummaryDTO model);//patch
-        ReturnModel EditProductTitle(EditProductTitleDTO model);//patch
+        Task<ReturnModel> UplaodProductImage(UploadImageDTO model, User user);//patch
+        Task<ReturnModel> EditProductPrice(EditProductDTO model, User user);//patch
+        Task<ReturnModel> EditProductSummary(EditProductSummaryDTO model);//patch
+        Task<ReturnModel> EditProductTitle(EditProductDTO model, User user);//patch
         Task<ReturnModel> TemporarilyDisableAProduct(string id);
     }
 }
