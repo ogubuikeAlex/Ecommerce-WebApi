01cb13250f8e18f34e3a6342c304f51213ff183e
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-07 20:56:06 +0100

    Added mothod to product Service

--- KingsStoreApi.Services/Interfaces/IProductService.cs
+++ KingsStoreApi.Services/Interfaces/IProductService.cs
@@ -15,7 +15,7 @@ namespace KingsStoreApi.Services.Interfaces
         Task<ReturnModel> BuyNow();
         ReturnModel UploadProduct(UploadProductDTO model);
         ReturnModel UplaodProductImage(UploadImageDTO model);//patch
-        ReturnModel EditProductPrice(EditProductPriceDTO model);//patch
+        ReturnModel EditProductPrice(EditProductDTO model);//patch
         ReturnModel EditProductSummary(EditProductSummaryDTO model);//patch
         ReturnModel EditProductTitle(EditProductTitleDTO model);//patch
         Task<ReturnModel> TemporarilyDisableAProduct(string id);
