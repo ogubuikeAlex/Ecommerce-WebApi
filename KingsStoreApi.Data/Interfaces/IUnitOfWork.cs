144184da097ba25d396445a277e70857dbd4a1d7
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-07 20:51:02 +0100

    Added savechangesAsync

--- KingsStoreApi.Data/Interfaces/IUnitOfWork.cs
+++ KingsStoreApi.Data/Interfaces/IUnitOfWork.cs
@@ -6,7 +6,8 @@ namespace KingsStoreApi.Data.Interfaces
 {
     public interface IUnitOfWork
     {
-        IRepository<T> GetRepository<T>() where T : class, IDelete;       
+        IRepository<T> GetRepository<T>() where T : class, IDelete;
+        Task SaveChangesAsync();
     }
 
     public interface IUnitOfWork<TContext> : IUnitOfWork { }
