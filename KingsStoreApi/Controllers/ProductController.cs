6d3273f2425508937d99c627aac451d631f49a6b
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-09 22:07:36 +0100

    Removed comments

--- KingsStoreApi/Controllers/ProductController.cs
+++ KingsStoreApi/Controllers/ProductController.cs
@@ -76,10 +76,6 @@ namespace KingsStoreApi.Controllers
 
 
 
-        /*
-         * 
-         * 
-         * EditProductTitle(EditProductDTO model, User user);//patch
-        Task<ReturnModel> TemporarilyDisableAProduct*/
+       
     }
 }
