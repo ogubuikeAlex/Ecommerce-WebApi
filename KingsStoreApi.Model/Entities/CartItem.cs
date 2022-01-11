5b5c2f100e753f942717b91196b3786e55bcb28d
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-11 23:17:07 +0100

    Update CartItems content

--- KingsStoreApi.Model/Entities/CartItem.cs
+++ KingsStoreApi.Model/Entities/CartItem.cs
@@ -8,6 +8,10 @@ namespace KingsStoreApi.Model.Entities
 {
     public class CartItem
     {
+       public Guid Id { get; set; }
 
+        public string CartId { get; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
     }
 }
