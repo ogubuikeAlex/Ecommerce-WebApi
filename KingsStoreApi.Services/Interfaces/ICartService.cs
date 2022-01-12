df705f2d2fb94a88015200f41481f510a79bc982
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-12 20:10:25 +0100

    Edited ICartService

--- KingsStoreApi.Services/Interfaces/ICartService.cs
+++ KingsStoreApi.Services/Interfaces/ICartService.cs
@@ -1,6 +1,4 @@
 ﻿using KingsStoreApi.Model.Entities;
-using KingsStoreApi.Model.Enums;
-using System;
 
 namespace KingsStoreApi.Services.Interfaces
 {
@@ -10,10 +8,7 @@ namespace KingsStoreApi.Services.Interfaces
         public void RemoveCartItem(string cartItemId);
         public void ClearCart();
         public decimal GetTotalCartPrice();
-        public Guid Id { get; set; }
-        public Guid? UserId { get; set; }
-        public string SessionId { get; set; }
-        public CartStatus CartStatus { get; set; }
+        
     }
 }
 
