1920f5e2b90777bc70c37b23dc51e382305dbcf1
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-12 20:13:58 +0100

    Updated Cart and CartItem

--- KingsStoreApi.Model/Entities/Cart.cs
+++ KingsStoreApi.Model/Entities/Cart.cs
@@ -1,9 +1,6 @@
 ﻿using KingsStoreApi.Model.Enums;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KingsStoreApi.Model.Entities
 {
--- KingsStoreApi.Model/Entities/CartItem.cs
+++ KingsStoreApi.Model/Entities/CartItem.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KingsStoreApi.Model.Entities
 {
     public class CartItem
     {
-       public Guid Id { get; set; }
-
+        public Guid Id { get; set; }
         public string CartId { get; }
         public Product Product { get; set; }
         public int Quantity { get; set; }
