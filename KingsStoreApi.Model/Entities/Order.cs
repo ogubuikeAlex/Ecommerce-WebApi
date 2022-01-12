086f53ebcab03d192fd1263b3e458e7075ad5aa5
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-12 20:35:08 +0100

    Added the content of order

--- KingsStoreApi.Model/Entities/Order.cs
+++ KingsStoreApi.Model/Entities/Order.cs
@@ -1,6 +1,20 @@
-﻿namespace KingsStoreApi.Model.Entities
+﻿using System.Collections.Generic;
+
+namespace KingsStoreApi.Model.Entities
 {
     public class Order
     {
+        public int ID { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+        public string UserID { get; set; }
+        public string Shipping { get; set; }
+        public int AddressID { get; set; }
+        public string OrderDate { get; set; }
+        public int TotalItemQty { get; set; }
+        public string DiscountName { get; set; }
+        public decimal DiscountPercent { get; set; }
+        public decimal DiscountAmt { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal Total { get; set; }
     }
 }
