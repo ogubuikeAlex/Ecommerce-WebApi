7f027870bb49b80eaa1f8ab24f19524c759edf6a
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-12 20:08:35 +0100

    Remove Cart Fields

--- KingsStoreApi.Services/Implementations/CartService.cs
+++ KingsStoreApi.Services/Implementations/CartService.cs
@@ -1,10 +1,6 @@
 ﻿using KingsStoreApi.Data.Interfaces;
 using KingsStoreApi.Model.Entities;
-using KingsStoreApi.Model.Enums;
 using KingsStoreApi.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace KingsStoreApi.Services.Implementations
 {
@@ -30,7 +26,7 @@ namespace KingsStoreApi.Services.Implementations
         }
         public void AddCartItem(Product product, int quantity)
         {
-            var cartItem = _CartContent.Where(p => p.Product.Id == product.Id).FirstOrDefault();
+            /*var cartItem = _repository.GetSingleByCondition(p => p.Product.Id == product.Id).FirstOrDefault();
 
             if (cartItem is null)
             {
@@ -40,29 +36,29 @@ namespace KingsStoreApi.Services.Implementations
             else
             {
                 cartItem.Quantity++;
-            }
+            }*/
         }
 
         public void RemoveCartItem(string cartItemId)
         {
-            var cartItem = _CartContent.Where(c => c.CartId == cartItemId).FirstOrDefault();
+           /* var cartItem = _CartContent.Where(c => c.CartId == cartItemId).FirstOrDefault();
 
             if (cartItem is not null)
-                _CartContent.Remove(cartItem);
+                _CartContent.Remove(cartItem);*/
         }
 
         public void ClearCart()
         {
-            _CartContent.Clear();
+            /*_CartContent.Clear();*/
         }
         public decimal GetTotalCartPrice()
         {
-            decimal totalPrice = 0;
+           decimal totalPrice = 0;
 
-            foreach (var item in _CartContent)
+            /* foreach (var item in _CartContent)
             {
                 item.Product.Price += totalPrice;
-            }
+            }*/
 
             return totalPrice;
         }       
