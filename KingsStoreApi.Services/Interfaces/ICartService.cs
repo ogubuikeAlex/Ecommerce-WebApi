df14b0a205aab32711b6ec47b1c4786f24365755
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-13 01:43:01 +0100

    Changed return type

--- KingsStoreApi.Services/Interfaces/ICartService.cs
+++ KingsStoreApi.Services/Interfaces/ICartService.cs
@@ -1,11 +1,12 @@
-﻿using KingsStoreApi.Model.DataTransferObjects.CartServiceDTO;
+﻿using KingsStoreApi.Helpers.Implementations;
+using KingsStoreApi.Model.DataTransferObjects.CartServiceDTO;
 using KingsStoreApi.Model.Entities;
 
 namespace KingsStoreApi.Services.Interfaces
 {
     public interface ICartService
     {
-        public void AddCartItem(AddToCartDTO model);
+        public ReturnModel AddCartItem(AddToCartDTO model);
         public void RemoveCartItem(string cartItemId);
         public void ClearCart();
         public decimal GetTotalCartPrice();        
