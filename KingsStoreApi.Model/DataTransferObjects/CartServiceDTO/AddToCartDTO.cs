28c6ed43f83a28173cb69545d8e79fd6087df273
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-13 01:11:23 +0100

    Created AddTOCartDTO

---
+++ KingsStoreApi.Model/DataTransferObjects/CartServiceDTO/AddToCartDTO.cs
@@ -0,0 +1,11 @@
+﻿using KingsStoreApi.Model.Entities;
+
+namespace KingsStoreApi.Model.DataTransferObjects.CartServiceDTO
+{
+    public class AddToCartDTO
+    {
+        public Cart Cart { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
