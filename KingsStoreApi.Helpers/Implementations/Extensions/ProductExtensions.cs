c258ee6a7174a1b46d491f29eed5648c2beb7343
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-13 09:27:46 +0100

    Added Extension method : Filter

---
+++ KingsStoreApi.Helpers/Implementations/Extensions/ProductExtensions.cs
@@ -0,0 +1,14 @@
+﻿using KingsStoreApi.Model.Entities;
+using System.Linq;
+
+namespace KingsStoreApi.Helpers.Implementations.Extensions
+{
+    public static class ProductExtensions 
+    {
+        public static IQueryable<Product> Filter (this IQueryable<Product> products, uint maxPrice, uint minPrice)
+        {
+            return products.Where(p => p.Price >= minPrice && p.Price <= maxPrice);
+        }
+        
+    }
+}
