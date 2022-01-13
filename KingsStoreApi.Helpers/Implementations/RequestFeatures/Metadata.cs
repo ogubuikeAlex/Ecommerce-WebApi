680592350934ae6379e0e54e0c3f0c75413d0e1e
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-13 08:30:45 +0100

    Added Metadata class to hold more info abour pagination

---
+++ KingsStoreApi.Helpers/Implementations/RequestFeatures/Metadata.cs
@@ -0,0 +1,18 @@
+﻿namespace KingsStoreApi.Helpers.Implementations.RequestFeatures
+{
+    public class Metadata
+    {
+        /// <summary>
+        /// CurrentPage: page we are on
+        /// TotalPages: after dividing totalcount by page size
+        /// TotalCount: Number of all our resourses
+        /// PageSize: how many resources we want on a page
+        /// </summary>
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+    }
+}
