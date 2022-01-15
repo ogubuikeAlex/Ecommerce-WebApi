188b57c1550da1e34f601bc4bbf8d721f8899063
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-15 18:45:09 +0100

    Changed to include property for decimal

---
+++ KingsStoreApi.Model/DataTransferObjects/ProductServiceDTO/EditProductPriceDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace KingsStoreApi.Model.DataTransferObjects.ProductServiceDTO
+{
+    public class EditProductPriceDTO
+    {
+        public string Id { get; set; }
+        public decimal NewValue { get; set; }
+    }
+}
