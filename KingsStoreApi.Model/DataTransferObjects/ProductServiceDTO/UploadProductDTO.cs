fa60ee9ed984510a5ea532278ac5d635a2513d68
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-15 04:24:33 +0100

    Added model propertied

--- KingsStoreApi.Model/DataTransferObjects/ProductServiceDTO/UploadProductDTO.cs
+++ KingsStoreApi.Model/DataTransferObjects/ProductServiceDTO/UploadProductDTO.cs
@@ -1,6 +1,13 @@
-﻿namespace KingsStoreApi.Model.DataTransferObjects.ProductServiceDTO
+﻿using System;
+
+namespace KingsStoreApi.Model.DataTransferObjects.ProductServiceDTO
 {
     public class UploadProductDTO
-    {
+    {     
+        
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
     }
 }
