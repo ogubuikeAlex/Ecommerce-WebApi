7d1d321a71499e5f6c62124ffb8b342c232c462b
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-15 18:57:37 +0100

    Create CartController

---
+++ KingsStoreApi/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using KingsStoreApi.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KingsStoreApi.Controllers
+{
+    [Route("api/Cart")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public Task<ReturnModel> AddCartItem(AddToCartDTO model);
+        Task<ReturnModel> RemoveCartItem(string cartItemId);
+        Task<ReturnModel> ClearCart(Cart cart);
+        ReturnModel GetTotalCartPrice(Cart cart);
+    }
+}
