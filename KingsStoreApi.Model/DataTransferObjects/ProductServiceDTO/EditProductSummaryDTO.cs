604ebf294f575f476b8031df2cb2509f656f4431
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-15 18:45:28 +0100

    removed

--- KingsStoreApi.Model/DataTransferObjects/ProductServiceDTO/EditProductSummaryDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KingsStoreApi.Model.DataTransferObjects.ProductServiceDTO
-{
-    public class EditProductSummaryDTO
-    {
-        public string Id { get; set; }
-        public string NewValue { get; set; }
-    }
-}
