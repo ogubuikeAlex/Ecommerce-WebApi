1a07610655cd9c48bc7e727326013fe0976e1b23
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-15 20:43:49 +0100

    Removed custom class properties and added already valuetypes

--- KingsStoreApi.Model/DataTransferObjects/CartServiceDTO/AddToCartDTO.cs
+++ KingsStoreApi.Model/DataTransferObjects/CartServiceDTO/AddToCartDTO.cs
@@ -4,8 +4,7 @@ namespace KingsStoreApi.Model.DataTransferObjects.CartServiceDTO
 {
     public class AddToCartDTO
     {
-        public Cart Cart { get; set; }
-        public Product Product { get; set; }
+        public string ProductId { get; set; }
         public int Quantity { get; set; }
     }
 }
