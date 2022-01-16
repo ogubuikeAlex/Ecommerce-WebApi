2b433b0de9e59ad1505634a9414fda135cd9bbb3
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-16 05:55:15 +0100

    Added getCartItems definition

--- KingsStoreApi.Services/Interfaces/ICartService.cs
+++ KingsStoreApi.Services/Interfaces/ICartService.cs
@@ -7,6 +7,7 @@ namespace KingsStoreApi.Services.Interfaces
 {
     public interface ICartService
     {
+        Task<ReturnModel> GetCartItems(); 
         Task<ReturnModel> AddCartItem(User user, string productId, int quantity);
         Task<ReturnModel> RemoveCartItem(string cartItemId);
         Task<ReturnModel> ClearCart(string userId);
