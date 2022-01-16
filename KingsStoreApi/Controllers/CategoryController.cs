ca1ff867c9e906376cf3ce6aaeeb362cbcf9d4b0
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-16 10:17:53 +0100

    Created CategoryController

---
+++ KingsStoreApi/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KingsStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        /*ReturnModel GetCategory(string categoryName);
+        ReturnModel GetAllCategories();
+        Task<ReturnModel> CreateCategory(CreateCategoryDTO model);
+        Task<ReturnModel> ToggleSoftDeleteCategory(string id);
+        Task<ReturnModel> UpdateCategoryTitle(UpdateCategoryDTO model);
+        Task<ReturnModel> UpdateCategorySummary(UpdateCategoryDTO model);*/
+    }
+}
