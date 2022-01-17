b44067ee6a05a584260455da825f697fc27505bd
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-17 21:44:18 +0100

    Adding mapping for representatinal DTOs

--- KingsStoreApi/Configuration/MappingProfile.cs
+++ KingsStoreApi/Configuration/MappingProfile.cs
@@ -1,19 +1,26 @@
 ﻿using AutoMapper;
+using KingsStoreApi.Model.DataTransferObjects.CartServiceDTO;
 using KingsStoreApi.Model.DataTransferObjects.CategoryServicesDTO;
 using KingsStoreApi.Model.DataTransferObjects.ProductServiceDTO;
+using KingsStoreApi.Model.DataTransferObjects.TransactionServiceDTO;
 using KingsStoreApi.Model.DataTransferObjects.UserServiceDTO;
 using KingsStoreApi.Model.Entities;
 
 namespace KingsStoreApi.Configuration
 {
     public class MappingProfile : Profile
-    {       
-            public MappingProfile()
-            {
-                // CreateMap<From, To>()
-                CreateMap<RegisterDTO, User>();
-                CreateMap<CreateCategoryDTO, Category>();
-                CreateMap<UploadProductDTO, Product>();
-            }       
+    {
+        public MappingProfile()
+        {
+            // CreateMap<From, To>()
+            CreateMap<RegisterDTO, User>();
+            CreateMap<CreateCategoryDTO, Category>();
+            CreateMap<UploadProductDTO, Product>();
+            CreateMap<CartItem, CartItemRepresentationalDTO>();
+            CreateMap<Cart, CartRepresentationalDTO>();
+            CreateMap<Product, ProductRepresentationalDTO>();
+            CreateMap<User, UserRepresentationalDTO>();
+            CreateMap<Transaction, TransactionRepresentationalDTO>();
+        }
     }
 }
