1dc494fa2862623fa051c3429faade52790e0cc0
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-17 21:48:19 +0100

    Updated CartrepresentationalDTO

--- KingsStoreApi.Model/DataTransferObjects/CartServiceDTO/CartRepresentationalDTO.cs
+++ KingsStoreApi.Model/DataTransferObjects/CartServiceDTO/CartRepresentationalDTO.cs
@@ -2,7 +2,7 @@
 
 namespace KingsStoreApi.Model.DataTransferObjects.CartServiceDTO
 {
-    class CartRepresentationalDTO
+    public class CartRepresentationalDTO
     {
         public string UserId { get; set; }
         public CartStatus CartStatus { get; set; }
