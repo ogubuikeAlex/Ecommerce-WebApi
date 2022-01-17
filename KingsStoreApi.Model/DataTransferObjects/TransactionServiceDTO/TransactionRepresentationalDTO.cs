56a42a81c7a4e8626510587b331390af65731247
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-17 21:22:14 +0100

    Completed TransactionRepresentationDTO

--- KingsStoreApi.Model/DataTransferObjects/TransactionServiceDTO/TransactionRepresentationalDTO.cs
+++ KingsStoreApi.Model/DataTransferObjects/TransactionServiceDTO/TransactionRepresentationalDTO.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using KingsStoreApi.Model.Enums;
+using System;
 
 namespace KingsStoreApi.Model.DataTransferObjects.TransactionServiceDTO
 {
-    class TransactionRepresentationalDTO
+    public class TransactionRepresentationalDTO
     {
+        public Guid UserId { get; set; }
+        public Guid OrderId { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public TransactionMode TransactionMode { get; set; }
+        public TransactionStatus TransactionStatus { get; set; }
     }
 }
