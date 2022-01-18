f4c7f56196c1a22e41e0390789b43c6e688d2a2d
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-18 21:18:55 +0100

    Created Method to get lineItemType array

--- KingsStoreApi.Services/Implementations/TransactionService.cs
+++ KingsStoreApi.Services/Implementations/TransactionService.cs
@@ -25,5 +25,24 @@ namespace KingsStoreApi.Services.Implementations
             };
         }
 
+        private lineItemType[] CreateLineItem (Order order)
+        {
+            var lineItems = new lineItemType[order.OrderItems.Count];
+            int count = 0;
+            foreach (var item in order.OrderItems)
+            {
+                //line items to process
+                lineItems[count] = new lineItemType
+                {
+                    itemId = (item.ProductID).ToString(),
+                    name = item.ProductName,
+                    quantity = item.Quantity,
+                    unitPrice = item.UnitPrice
+                };
+                count++;
+            }
+
+            return lineItems;
+        }
     }
 }
