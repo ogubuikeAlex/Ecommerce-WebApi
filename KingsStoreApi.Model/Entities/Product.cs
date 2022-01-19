40e26e91b3f2608277fe7f5b4469e531e204546d
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-19 18:38:27 +0100

    Update

--- KingsStoreApi.Model/Entities/Product.cs
+++ KingsStoreApi.Model/Entities/Product.cs
@@ -8,16 +8,9 @@ namespace KingsStoreApi.Model.Entities
         public string UserId { get; set; }
         public string Title { get; set; }
         public string Summary { get; set; }
-        public decimal Price { get; set; }
-      
+        public decimal Price { get; set; }      
         public int Quantity { get; set; }
         public DateTime PublishedAt { get; set; }
         public byte[] ProductImage { get; set; }
-
-       
-
-        //Store date of isdeleted
-        //if after 30 dyas product is still isdeleted
-        //turn on 
     }
 }
