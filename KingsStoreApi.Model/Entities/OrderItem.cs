4174fbf6444dbeb20e5e2ab5d2bc80b7c6619fbe
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-19 19:32:33 +0100

    Converted Ids to strings

--- KingsStoreApi.Model/Entities/OrderItem.cs
+++ KingsStoreApi.Model/Entities/OrderItem.cs
@@ -6,8 +6,8 @@ namespace KingsStoreApi.Model.Entities
     public class OrderItem : Entity
     {
         public Guid ID { get; set; }
-        public int OrderID { get; set; }
-        public int ProductID { get; set; }
+        public string OrderID { get; set; }
+        public string ProductID { get; set; }
         public Product Product { get; set; }        
         public int Quantity { get; set; }    
         
