9d5baa138bb05ec310c12c1ee4d4d85249bcec4e
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-19 18:47:21 +0100

    Initial addition to address entity

--- KingsStoreApi.Model/Entities/Address.cs
+++ KingsStoreApi.Model/Entities/Address.cs
@@ -1,6 +1,15 @@
-﻿namespace KingsStoreApi.Model.Entities
+﻿using System;
+
+namespace KingsStoreApi.Model.Entities
 {
     public class Address
     {
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public string Country { get; set; }
+        public string HouseNumber { get; set; }
+        public string street { get; set; }
+        public string City { get; set; }
+
     }
 }
