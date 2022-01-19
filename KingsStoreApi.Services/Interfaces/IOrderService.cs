5d3c0860707a4dc673cae53aecef2f8dbc534eec
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-19 18:17:29 +0100

    Added contarct methods to IOrderService inteface

--- KingsStoreApi.Services/Interfaces/IOrderService.cs
+++ KingsStoreApi.Services/Interfaces/IOrderService.cs
@@ -1,6 +1,15 @@
-﻿namespace KingsStoreApi.Services.Interfaces
+﻿using KingsStoreApi.Helpers.Implementations;
+using KingsStoreApi.Model.Entities;
+using System.Threading.Tasks;
+
+namespace KingsStoreApi.Services.Interfaces
 {
     public interface IOrderService
     {
+        ReturnModel GetOrderItems(string userId);
+        Task<ReturnModel> AddOrderItem(User user, string productId, int quantity);
+        Task<ReturnModel> RemoveOrderItem(string OrderItemId);
+        Task<ReturnModel> ClearOrder(string userId);
+        ReturnModel GetTotalOrderPrice(string userId);
     }
 }
