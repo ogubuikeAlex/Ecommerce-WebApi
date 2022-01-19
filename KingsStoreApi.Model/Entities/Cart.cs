104790696aa98266a11e9a143297b620321a4eab
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-19 19:43:53 +0100

    Added CartItems to Cart

--- KingsStoreApi.Model/Entities/Cart.cs
+++ KingsStoreApi.Model/Entities/Cart.cs
@@ -6,9 +6,14 @@ namespace KingsStoreApi.Model.Entities
 {
     public class Cart : Entity
     {
+        public Cart()
+        {
+            CartItems = new List<CartItem>();
+        }
         public Guid Id { get; set; }
         public string UserId { get; set; }
         public string SessionId { get; set; }
-        public CartStatus CartStatus { get; set; }        
+        public CartStatus CartStatus { get; set; } 
+        public List<CartItem> CartItems { get; set; }
     }
 }
