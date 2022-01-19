4dc5327a16dde905fe4d6fa3c9fdf8e5e3a6374b
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-19 18:38:17 +0100

    Updated OrderItem

--- KingsStoreApi.Model/Entities/OrderItem.cs
+++ KingsStoreApi.Model/Entities/OrderItem.cs
@@ -1,16 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace KingsStoreApi.Model.Entities
 {
     public class OrderItem
     {
-        public int ID { get; set; }
+        public Guid ID { get; set; }
         public int OrderID { get; set; }
         public int ProductID { get; set; }
-        public string UserID { get; set; }
-        public string ProductName { get; set; }
-        public int Quantity { get; set; }
-        public string ImgUrl { get; set; }
-        public decimal UnitPrice { get; set; }
+        public Product Product { get; set; }        
+        public int Quantity { get; set; }    
+        
     }
 }
