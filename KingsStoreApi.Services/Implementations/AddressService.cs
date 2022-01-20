a7c7b93368f511c4e621a32235f1877528cdc8e7
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-20 12:42:33 +0100

    Updated to IAdressService

---
+++ KingsStoreApi.Services/Implementations/AddressService.cs
@@ -0,0 +1,19 @@
+﻿using KingsStoreApi.Helpers.Implementations;
+using KingsStoreApi.Model.DataTransferObjects.AddressServiceDTO;
+using KingsStoreApi.Services.Interfaces;
+
+namespace KingsStoreApi.Services.Implementations
+{
+    public class AddressService : IAddressService
+    {
+        public ReturnModel AddAddress(AddAddressDTO model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ReturnModel RemoveAddress(RemoveAddressDTO model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
