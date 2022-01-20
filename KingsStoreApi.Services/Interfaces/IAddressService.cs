d501106e56585eeb77db101dafe7f44410069127
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-20 12:43:28 +0100

    Update to name

--- KingsStoreApi.Services/Interfaces/IAddressService.cs
+++ KingsStoreApi.Services/Interfaces/IAddressService.cs
@@ -3,7 +3,7 @@ using KingsStoreApi.Model.DataTransferObjects.AddressServiceDTO;
 
 namespace KingsStoreApi.Services.Interfaces
 {
-    public interface AddressService
+    public interface IAddressService
     {
         ReturnModel AddAddress(AddAddressDTO model);
         ReturnModel RemoveAddress(RemoveAddressDTO model);
