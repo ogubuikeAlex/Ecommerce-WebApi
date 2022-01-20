82df69dde4d078bfc8ddcafa083c4ffa8ee7a0bb
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-20 20:43:59 +0100

    Added Discount entity

---
+++ KingsStoreApi.Model/Entities/Discount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KingsStoreApi.Model.Entities
+{
+    public class Discount : Entity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public byte PercentageOff { get; set; }
+        public int NumberOfBenefeciaries { get; set; }
+       
+    }
+}
