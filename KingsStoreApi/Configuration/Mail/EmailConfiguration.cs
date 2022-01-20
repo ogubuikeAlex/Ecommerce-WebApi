fd1172459d32afaf4048edc5497baa9608e75e60
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-20 21:40:55 +0100

    Added properties to emailConfiguration

--- KingsStoreApi/Configuration/Mail/EmailConfiguration.cs
+++ KingsStoreApi/Configuration/Mail/EmailConfiguration.cs
@@ -7,5 +7,10 @@ namespace KingsStoreApi.Configuration.Mail
 {
     public class EmailConfiguration
     {
+        public string From { get; set; }
+        public string SmtpServer { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
