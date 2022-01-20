546736ff905eb5475de12f440ab79f9c23fd6f55
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-20 13:43:02 +0100

    Added enum for Discounts

---
+++ KingsStoreApi.Model/Enums/Discounts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingsStoreApi.Model.Enums
+{
+    public enum Discounts
+    {
+        OwambeBlockChain = 5,
+        BigBoyBroom = 10 ,
+        KpomoKatrina = 15,
+        Chiefkpafuka = 20,
+        OgaKpakpata = 50,
+        OshofreeGettat = 100,
+    }
+}
