e9ac7702fdb6c51a70ea80e2b7b368964cf53864
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-22 05:11:49 +0100

    Updated IEMailsender implementations

---
+++ KingsStoreApi/Configuration/Mail/EmailSender.cs
@@ -0,0 +1,20 @@
+﻿using MimeKit;
+
+namespace KingsStoreApi.Configuration.Mail
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailConfiguration _emailConfig;
+
+        public EmailSender(EmailConfiguration emailConfig)
+        {
+            _emailConfig = emailConfig;
+        }
+
+        public void SendEmail(Message message)
+        {
+            var emailMessage = CreateEmailMessage(message);
+            Send(emailMessage);
+        }
+    }
+}
