b3f123b68d48aa6dfea4e3fbafd39350da94cc7d
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-22 21:20:07 +0100

    Update AdAddress to have user

--- KingsStoreApi.Services/Interfaces/IAddressService.cs
+++ KingsStoreApi.Services/Interfaces/IAddressService.cs
@@ -1,11 +1,12 @@
 ﻿using KingsStoreApi.Helpers.Implementations;
 using KingsStoreApi.Model.DataTransferObjects.AddressServiceDTO;
+using KingsStoreApi.Model.Entities;
 
 namespace KingsStoreApi.Services.Interfaces
 {
     public interface IAddressService
     {
-        ReturnModel AddAddress(AddAddressDTO model);
+        ReturnModel AddAddress(AddAddressDTO model, User user);
         ReturnModel RemoveAddress(RemoveAddressDTO model);
     }
 }
