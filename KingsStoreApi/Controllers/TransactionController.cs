ca2daaddf25a1657b23cc6960d193922c4f5f0e2
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-01-22 21:51:24 +0100

    Added TransactionController

---
+++ KingsStoreApi/Controllers/TransactionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KingsStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+    }
+}
