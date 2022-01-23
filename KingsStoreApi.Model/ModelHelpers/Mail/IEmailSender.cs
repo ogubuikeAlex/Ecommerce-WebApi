0a848fe47f4adcf3d74a3f4fe893dc762f77ad8b
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-01-23 22:57:26 +0100

    Added IEmailSender to model project

---
+++ KingsStoreApi.Model/ModelHelpers/Mail/IEmailSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingsStoreApi.Model.ModelHelpers.Mail
+{
+    internal interface IEmailSender
+    {
+        void SendEmail(Message message);
+    }
+}
