542d88d06dcd94898e568edda452b745356c4922
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-01-23 22:58:38 +0100

    Added Email Sneder a concrete implementation of IEmail

---
+++ KingsStoreApi.Model/ModelHelpers/Mail/EmailSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KingsStoreApi.Model.ModelHelpers.Mail
+{
+    internal class EmailSender : IEmailSender
+    {
+        public void SendEmail(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
