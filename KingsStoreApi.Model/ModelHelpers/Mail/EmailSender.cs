d93c68be192ad7ada6bd05e948fbbe5e085cba0b
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-01-23 23:02:49 +0100

    Added CreateEmailMeassage private method

--- KingsStoreApi.Model/ModelHelpers/Mail/EmailSender.cs
+++ KingsStoreApi.Model/ModelHelpers/Mail/EmailSender.cs
@@ -8,9 +8,27 @@ namespace KingsStoreApi.Model.ModelHelpers.Mail
 {
     internal class EmailSender : IEmailSender
     {
+        private readonly EmailConfiguration _emailConfig;
+
+        public EmailSender(EmailConfiguration emailConfig)
+        {
+            _emailConfig = emailConfig;
+        }
+
         public void SendEmail(Message message)
         {
-            throw new NotImplementedException();
+            var emailMessage = CreateEmailMessage(message);
+            Send(emailMessage);
+        }
+
+        private MimeMessage CreateEmailMessage(Message message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(_emailConfig.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+            return emailMessage;
         }
     }
 }
