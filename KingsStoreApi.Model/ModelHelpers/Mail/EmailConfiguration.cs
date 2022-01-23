975dd04113b28ee211ce33bcbcf2d25fa258d097
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-01-23 23:05:39 +0100

    Updated EmailConfiguration

--- KingsStoreApi.Model/ModelHelpers/Mail/EmailConfiguration.cs
+++ KingsStoreApi.Model/ModelHelpers/Mail/EmailConfiguration.cs
@@ -8,5 +8,10 @@ namespace KingsStoreApi.Model.ModelHelpers.Mail
 {
     internal class EmailConfiguration
     {
+        public string From { get; set; }
+        public string SmtpServer { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
