75f86a3c083a69fe7bcd6e89a5571bfd2728f9dc
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-01-23 23:03:43 +0100

    Add private send method

--- KingsStoreApi.Model/ModelHelpers/Mail/EmailSender.cs
+++ KingsStoreApi.Model/ModelHelpers/Mail/EmailSender.cs
@@ -30,5 +30,29 @@ namespace KingsStoreApi.Model.ModelHelpers.Mail
             emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
             return emailMessage;
         }
+
+        private void Send(MimeMessage mailMessage)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect(_emailConfig.SmtpServer, _emailConfig.Port, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate(_emailConfig.UserName, _emailConfig.Password);
+                    client.Send(mailMessage);
+                }
+                catch
+                {
+                    //log an error message or throw an exception or both.
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+            }
+        }
     }
 }
