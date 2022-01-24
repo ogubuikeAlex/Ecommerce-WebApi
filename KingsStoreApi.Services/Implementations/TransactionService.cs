889f9fcedcf08921ec37e60ad89fbc158e2a0586
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-01-24 23:53:47 +0100

    check for failed transactions

--- KingsStoreApi.Services/Implementations/TransactionService.cs
+++ KingsStoreApi.Services/Implementations/TransactionService.cs
@@ -77,6 +77,9 @@ namespace KingsStoreApi.Services.Implementations
             if (result.Contains("not found"))
                 return new ReturnModel { Message = result };
 
+            if (result.Contains("transactionFailed"))
+                return new ReturnModel { Success = false };
+
             return new ReturnModel { Message = "successful", Success = true};
         }
 
